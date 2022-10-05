2131e6c1ab52f4ba95ac759e62b6250fbc94e717
Chen <chenc10@126.com>
2022-10-05 21:11:58 +0800

    two-level bsp sync

--- edge_process.py
+++ edge_process.py
@@ -7,32 +7,38 @@ from rpyc.utils.server import ThreadedServer
 
 CUDA = torch.cuda.is_available()
 
-class AggregationService(Service):
-    def __init__(self, world_size):
+class EdgeAggregationService(Service):
+    def __init__(self, world_size, server_ip, server_port, edge_rank):
         self.world_size = world_size
-        self.tensor_lists_collected = [0] * world_size
-        self.has_arrived = [0] * world_size
+        self.edge_rank = edge_rank
+        self.edge_round_id = 0
         self.aggregated_tensor_list = None
 
-        self.report_time_lists = [[] for i in range(world_size)]
-        self.next_pull_time = 0
-        self.next_pull_rank = 0
+        self.tensor_lists_collected = [0] * world_size
+        self.has_arrived = [0] * world_size
+
+        ''' connect to the FL server '''
+        rpyc_config = rpyc.core.protocol.DEFAULT_CONFIG
+        rpyc_config['allow_pickle'] = True
+        self.connection = rpyc.connect(server_ip, server_port, config=rpyc_config)
 
     def logging(self, string):
-        print('['+str(datetime.datetime.now())+'] [Server] '+str(string))
+        print('['+str(datetime.datetime.now())+'] [Edge] '+str(string))
         sys.stdout.flush()
 
     def exposed_aggregate(self, rank, round_id, tensor_list):
         self.logging('receive rank %d, round_id %d' % (rank, round_id))
         self.tensor_lists_collected[rank] = rpyc.classic.obtain(tensor_list)
         self.has_arrived[rank] = 1
+        self.edge_round_id = max(self.edge_round_id, round_id)
 
         ''' handle the case when all the clients have reported '''
         if sum(self.has_arrived) == self.world_size:
             ''' make aggregation and conduct potential analysis '''
-            self.aggregated_tensor_list = self.calculate_model_average()
-            self.next_pull_time = 0
-            self.next_pull_rank = 0
+            edge_aggregated_tensor_list = self.calculate_model_average()
+
+            ''' report the local average to the global server '''
+            self.aggregated_tensor_list = self.sync_with_global_server(edge_aggregated_tensor_list)
 
             ''' reset '''
             self.has_arrived = [0] * self.world_size
@@ -57,20 +63,30 @@ class AggregationService(Service):
         
         return aggregated_tensor_list
 
+    def sync_with_global_server(self, edge_aggregated_tensor_list):
+        tensor_list_received = self.connection.root.aggregate(self.edge_rank, self.edge_round_id, edge_aggregated_tensor_list)
+        global_tensor_list = [rpyc.classic.obtain(i).cuda() for i in tensor_list_received] if CUDA \
+            else [rpyc.classic.obtain(i) for i in tensor_list_received]
+        return global_tensor_list
+
+
 if __name__ == "__main__":
 
     ''' Parse arguments and create APF profile. '''
     parser = argparse.ArgumentParser()
-    parser.add_argument('--port', type=int, default=20000)
-    parser.add_argument('--edge_number', type=int, default=5)
+    parser.add_argument('--server_ip', type=str, default='127.0.0.1')
+    parser.add_argument('--server_port', type=int, default=20000)
+    parser.add_argument('--edge_rank', type=int, default=0)
+    parser.add_argument('--port', type=int, default=30000)
+    parser.add_argument('--client_number', type=int, default=5)
     parser.add_argument('--trial_no', type=int, default=0)
     parser.add_argument('--remarks', type=str, default='Remarks Missing...')
 
     args = parser.parse_args()
     print('Trial ID: ' + str(args.trial_no) + '; Exp Setup Remarks: ' + args.remarks + '\n')
 
-    service = AggregationService(args.edge_number)
+    service = EdgeAggregationService(args.client_number, args.server_ip, args.server_port, args.edge_rank)
     rpyc_config = rpyc.core.protocol.DEFAULT_CONFIG
     rpyc_config['allow_pickle'] = True
-    server = ThreadedServer(service, port=args.port, protocol_config=rpyc_config)
-    server.start()
+    edge = ThreadedServer(service, port=args.port, protocol_config=rpyc_config)
+    edge.start()
--- train.sh
+++ train.sh
@@ -1,20 +1,13 @@
-### set home directory
+### set home directory, server, edge and clients
 home='/root/adaptive_freezing'
+server='localhost'
+edges='localhost localhost'
+echo 'Server_host: '$server
+echo 'Edge_hosts: '$edges
 
-### prepare master and workers
-master='localhost'
-workers='localhost localhost'
-echo 'master(coordinator): '$master
-echo 'worker_hosts: '$workers
-world_size=0
-for i in $workers
-do
-		world_size=$((world_size+1))
-done
-
-### create log dir and code snapshot
+### ---- create log dir and code snapshot
 read -p "Enter exp-setup notes: " remarks
-read -p "Specify allocated GPU-ID (world_size: $world_size): " cuda 
+read -p "Specify allocated GPU-ID: " cuda 
 trial_no=$(ls $home/Logs/ | wc -l)
 log_dir=$home/Logs/${trial_no}_$remarks
 
@@ -25,16 +18,56 @@ cp -r $home/models $log_dir/code_snapshot
 rm -f Latest_Log && ln -s $log_dir Latest_Log
 echo 'logs in '$log_dir
 
-### launch server process
-command="export CUDA_VISIBLE_DEVICES=$cuda && python3 $home/server_process.py --world_size=$world_size --server_port=$((20000+trial_no)) --remarks=$remarks --trial_no=$trial_no"
-nohup ssh $master $command > $log_dir/server.log 2>&1 &
-
-### launch worker process
-num=0
-for i in $workers
+### ---- launch server process
+edge_number=0
+for e_ip in $edges
 do
-	command="export CUDA_VISIBLE_DEVICES=$cuda && python3 $home/worker_process.py --server_ip=$master --server_port=$((20000+trial_no)) --rank=$num --world_size=$world_size --remarks=$remarks --trial_no=$trial_no"
+		edge_number=$((edge_number+1))
+done
+command="export CUDA_VISIBLE_DEVICES=$cuda && python3 $home/server_process.py \
+	--port=$((20000+trial_no)) --edge_number=$edge_number \
+	--remarks=$remarks --trial_no=$trial_no"
+echo "launch server:"
+echo $command
+echo 
+nohup ssh $server $command > $log_dir/server.log 2>&1 &
+sleep 1
+
+### ---- launch edge process and the assigned clients one by one
+edge_serial_num=0
+for e_ip in $edges
+do
+	clients='localhost localhost'
+
+	client_number=0
+	for c_ip in $clients
+	do
+			client_number=$((client_number+1))
+	done
+
+	command="export CUDA_VISIBLE_DEVICES=$cuda && python3 $home/edge_process.py \
+		--server_ip=$server --server_port=$((20000+trial_no)) --edge_rank=$edge_serial_num \
+		--port=$((30000 + trial_no + 10*edge_serial_num)) --client_number=$client_number \
+		--remarks=$remarks --trial_no=$trial_no"
+	echo "launch edge "$edge_serial_num": "
 	echo $command
-	nohup ssh $i $command > $log_dir/worker_$num.log 2>&1 &
-	num=$((num+1))
+	nohup ssh $e_ip $command > $log_dir/edge_$edge_serial_num.log 2>&1 &
+	sleep 1
+
+
+	### respectively launch each client for this edge
+	client_serial_num=0
+	for c_ip in $clients
+	do
+		command="export CUDA_VISIBLE_DEVICES=$cuda && python3 $home/worker_process.py \
+			--server_ip=$e_ip --server_port=$((30000 + trial_no + 10*edge_serial_num)) --rank=$client_serial_num --world_size=$client_number\
+			--remarks=$remarks --trial_no=$trial_no"
+		echo "launch client "${edge_serial_num}"-"${client_serial_num}": "
+		echo $command
+		nohup ssh $c_ip $command > $log_dir/client_${edge_serial_num}_${client_serial_num}.log 2>&1 &
+		client_serial_num=$((client_serial_num+1))
+	done
+
+	edge_serial_num=$((edge_serial_num+1))
+	echo
 done
